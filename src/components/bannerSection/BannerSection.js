1473049e3ca3fd65b467df8cd142973fa53ce3c7
irfan dayan <engr.irfandayan@gmail.com>
2022-03-03 22:00:54 +0500

    Finished home banner section, Add second item

--- src/components/bannerSection/BannerSection.js
+++ src/components/bannerSection/BannerSection.js
@@ -6,6 +6,7 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 
 import Img1 from "../../assets/images/1.jpeg";
+import Img2 from "../../assets/images/1.jpeg";
 
 const BannerSection = () => {
   return (
@@ -16,8 +17,8 @@ const BannerSection = () => {
         items="1"
         dots={false}
       >
+        {/* Slide Item #1 */}
         <div className="slide-item">
-          {/* <img src={Img1} alt="" /> */}
           <div
             className="image-layer"
             style={{ backgroundImage: `url(${Img1})` }}
@@ -34,6 +35,29 @@ const BannerSection = () => {
                 </h1>
                 <div className="lower-subtitle textured-text">
                   - Only 500 keys available -
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+        {/* Slide Item #2 */}
+        <div className="slide-item">
+          <div
+            className="image-layer"
+            style={{ backgroundImage: `url(${Img2})` }}
+          ></div>
+
+          <Container className="d-flex justify-content-center align-items-center">
+            <div className="content-box">
+              <div className="content">
+                <div className="upper-subtitle textured-text">
+                  - NFT Freaks x Mentalist421 -
+                </div>
+                <h1>
+                  <span className="textured-text">NFT Academy</span>
+                </h1>
+                <div className="lower-subtitle textured-text">
+                  - Only 1500 keys available -
                 </div>
               </div>
             </div>
