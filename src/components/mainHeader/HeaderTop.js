3f968244830ee0df924dcef5282c5cede0541efe
irfan dayan <engr.irfandayan@gmail.com>
2022-03-03 09:17:04 +0500

    Fix bem classes

--- src/components/mainHeader/HeaderTop.js
+++ src/components/mainHeader/HeaderTop.js
@@ -11,14 +11,22 @@ const HeaderTop = () => {
             <div class="header-top__left-text">Welcome to the NFT Academy</div>
           </div>
           <div className="header-top__right">
-            <ul class="header-top__socials">
-              <li>
-                <a href="http://www.twitter.com/nftfreaks" target="_blank">
+            <ul className="header-top__social">
+              <li className="header-top__social-item">
+                <a
+                  className="header-top__social-link"
+                  href="http://www.twitter.com/nftfreaks"
+                  target="_blank"
+                >
                   @nftfreaks
                 </a>
               </li>
-              <li>
-                <a href="http://www.twitter.com/mentalist420" target="_blank">
+              <li className="header-top__social-item">
+                <a
+                  className="header-top__social-link"
+                  href="http://www.twitter.com/mentalist420"
+                  target="_blank"
+                >
                   @mentalist420
                 </a>
               </li>
