2d66f5f42d9d0cc94fe5d6a17f4588e28a71372e
irfan dayan <engr.irfandayan@gmail.com>
2022-03-03 16:04:52 +0500

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import logo from "./logo.svg";
 
+import { useState, useEffect } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
+
 import "./App.css";
 import MainHeader from "./components/mainHeader/MainHeader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
-      <MainHeader />
+      {loading ? (
+        <div className="d-flex justify-content-center align-items-center vh-100">
+          <ClipLoader color={"#ff7a21"} loading={loading} size={45} />
+        </div>
+      ) : (
+        <MainHeader />
+      )}
     </>
   );
 }
