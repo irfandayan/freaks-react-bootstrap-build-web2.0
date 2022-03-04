0cdd528b6c3724481f8dc49603da7ff28e6aa077
irfan dayan <engr.irfandayan@gmail.com>
2022-03-04 16:38:28 +0500

    Add game rewards

---
+++ src/components/gameViewSection/AwardBlock.js
@@ -0,0 +1,15 @@
+import { Col } from "react-bootstrap";
+import "./AwardBlock.css";
+
+const AwardBlock = ({ upperTitle, lowerTitle }) => {
+  return (
+    <Col lg={3} className="award-block">
+      <div className="award-block__inner">
+        <div className="upper-title">{upperTitle}</div>
+        <div className="lower-title">{lowerTitle}</div>
+      </div>
+    </Col>
+  );
+};
+
+export default AwardBlock;
--- src/components/gameViewSection/GameAwards.js
+++ src/components/gameViewSection/GameAwards.js
@@ -1,7 +1,34 @@
+import { Row } from "react-bootstrap";
+import AwardBlock from "./AwardBlock";
 import "./GameAwards.css";
 
 const GameAwards = () => {
-  return <h1>Awards are coming</h1>;
+  return (
+    <div className="game-awards">
+      <div class="sec-title centered">
+        <h2>4 Levels of Admission</h2>
+        <span class="bottom-curve"></span>
+      </div>
+
+      <div className="awards">
+        <Row className="clearfix">
+          <AwardBlock upperTitle="Level One" lowerTitle="Exclusive Community" />
+          <AwardBlock
+            upperTitle="Level Two"
+            lowerTitle="Level 1+ VIP Channels"
+          />
+          <AwardBlock
+            upperTitle="Level Three"
+            lowerTitle="Level 1+2 Dev Access"
+          />
+          <AwardBlock
+            upperTitle="Level Four"
+            lowerTitle="Level 1 + 2 + 3 Direct Access"
+          />
+        </Row>
+      </div>
+    </div>
+  );
 };
 
 export default GameAwards;
